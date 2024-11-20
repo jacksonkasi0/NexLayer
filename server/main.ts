95e1de495340bfb9a416a8fe5e24d9bb38fbfce9
jacksonkasi0 <jacksonkasi@outlook.com>
2024-11-20 21:09:51 +0530

    enable cors to all domain

--- server/main.ts
+++ server/main.ts
@@ -1,10 +1,13 @@
 import { Hono } from "@hono/hono";
+import { cors } from '@hono/hono/cors'
 
 // ** import routes
 import { routes } from "@/routers/index.ts";
 
 const app = new Hono();
 
+
+app.use('*', cors())
 app.get("/", (c) => c.text("Hello NextLayer⚡"));
 
 app.route("/api", routes);
